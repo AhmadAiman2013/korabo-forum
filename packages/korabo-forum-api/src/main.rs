465f8e13d8e28f16cec84ac28484ae484b8c8414
ahmad aiman <ahmadaimanalias@gmail.com>
2026-07-26 18:36:14 +0800

    fix cors

--- packages/korabo-forum-api/src/main.rs
+++ packages/korabo-forum-api/src/main.rs
@@ -85,8 +85,9 @@ async fn main() -> Result<(), Error> {
     let queue = SqsClient::new(sqs, queue_url_noti, queue_url_cm_del);
 
     let origins = [
-        "https://d3h6bl8rffsevw.cloudfront.net".parse()?,
-        "http://localhost:4200".parse()?,
+        "http://localhost:5173".parse()?,
+        "https://koraboweb.online".parse()?,
+        "https://d-2rw4lmweh4.execute-api.ap-southeast-1.amazonaws.com".parse()?,
     ];
 
     let cors = CorsLayer::new()
@@ -98,7 +99,9 @@ async fn main() -> Result<(), Error> {
             Method::DELETE,
             Method::OPTIONS,
         ])
-        .allow_headers([CONTENT_TYPE, AUTHORIZATION]);
+        .allow_headers([CONTENT_TYPE, AUTHORIZATION])
+        .allow_credentials(true);
+
 
     let state = AppState {
         repo,
